60ac954d6400342ce401525d445fa8deaf4ad182
Awashhh <dddrag734@gmail.com>
2024-05-20 00:47:32 +0300

    change base url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://merkin-server.nomoredomainswork.ru/admin/dashboard'
+export const BASE_URL = 'https://merkin-server.nomoredomainswork.ru/api'
 
 export const endpoints = {
 	games: `${BASE_URL}/games`,
